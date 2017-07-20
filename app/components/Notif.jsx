bd05fdc2d2b7edda6bdbfcf5694eb312818d0d36
Ralex86 <hassler@monemail.com>
2017-07-20 06:59:13 +0200

    adding latest submission notif

--- app/components/Notif.jsx
+++ app/components/Notif.jsx
@@ -3,14 +3,27 @@ import FaClose from 'react-icons/lib/fa/close'
 
 import styles from './../css/notif.css'
 
+import 'whatwg-fetch'
 
 export default class Notif extends Component{
     constructor(props){
         super(props)
         this.state={
-            notif: true
+            notif: true,
+            list: null
         }
         this.handleClick = this.handleClick.bind(this)
+    }
+
+    componentDidMount(){
+        fetch('http://alexandre.hassler.fr:3000/latest')
+            .then(res => res.json())
+            .then(list => {
+                this.setState({
+                    list: list
+                })
+            })
+            .catch(err => console.log(err))
     }
 
     handleClick(){
@@ -22,13 +35,22 @@ export default class Notif extends Component{
     }
 
     render(){
-        
+       const {list} = this.state 
+        console.log(list)
         return(
             this.state.notif ? (
                 <div className={styles.notif}>
-                    <h2>Updates (19/08/17)</h2>
-                    <p>You can finally add youtube videos !! Please try to copy paste a valid youtube URL !</p>
-                    <p><a href="mailto:hassler@monemail.com">Alexandre Hassler</a></p>
+                    <h2>Latest videos submissions</h2>
+                    <ul>
+                        {list ? (
+                            list.map((tune,index) => (
+                                <li key={index}>
+                                    <span> { tune.title } </span>
+                                    <span> { tune.CreatedOn.slice(0,10) } </span>
+                                </li>
+                            ))
+                        ) : <span>loading</span>}
+                    </ul>
                     <FaClose className={styles.notif_close} onClick={() => this.handleClick()}/>
                 </div>
             ) : null
